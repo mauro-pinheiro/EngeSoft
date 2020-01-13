5cc5b5c62730342472aff551ded510b860d5faa6
Mauro Campos <38911137+mauro-pinheiro@users.noreply.github.com>
2020-01-13 08:44:10 -0300

    pagebody move left

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -11,6 +12,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -25,6 +27,7 @@
     <link href="{{ asset('css/pagebody.css') }}" rel="stylesheet">
     @yield('css')
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md topbar shadow-sm">
@@ -32,7 +35,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -46,72 +51,86 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                    style="display: none;">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
             </div>
         </nav>
 
-            <input type='checkbox' id='check'>
-            <label for='check'>
-                <i class="fas fa-bars" id="btn"></i>
-                <i class="fas fa-times" id="cancel"></i>
-            </label>
-            <nav class='sidebar'>
-                <header>{{config('app.name', 'Laravel')}}</header>
-                <ul>
-                    <li>
-                        <a href=""><i class="fas fa-users"></i> Perfil</a>
-                    </li>
-                    <li>
+        <input type='checkbox' id='check' onchange="move();">
+        <label for='check'>
+            <i class="fas fa-bars" id="btn"></i>
+            <i class="fas fa-times" id="cancel"></i>
+        </label>
+        <nav class='sidebar'>
+            <header>{{config('app.name', 'Laravel')}}</header>
+            <ul>
+                <li>
+                    <a href=""><i class="fas fa-users"></i> Perfil</a>
+                </li>
+                <li>
                     <a href="{{url('editions')}}"><i class="far fa-newspaper"></i> Edicões</a>
-                    </li>
-                    <li>
-                        <a href=""><i class="far fa-star"></i> Avaliações</a>
-                    </li>
-                    <li>
-                        <a href=""><i class="far fa-question-circle"></i> Sobre</a>
-                    </li>
-                    <li>
-                        <a href=""><i class="fas fa-cog"></i> Configurações</a>
-                    </li>
-                    <li>
-                        <a href=""><i class="fas fa-envelope"></i> Contato</a>
-                    </li>
-                </ul>
-            </div>
-
-        <main class="pagebody">
-            @yield('content')
-        </main>
-
-        <footer id='footer'>Grupo EngeSoft | Analise 2<br>2019.2</footer>
+                </li>
+                <li>
+                    <a href=""><i class="far fa-star"></i> Avaliações</a>
+                </li>
+                <li>
+                    <a href=""><i class="far fa-question-circle"></i> Sobre</a>
+                </li>
+                <li>
+                    <a href=""><i class="fas fa-cog"></i> Configurações</a>
+                </li>
+                <li>
+                    <a href=""><i class="fas fa-envelope"></i> Contato</a>
+                </li>
+            </ul>
     </div>
+
+    <main class="pagebody">
+        @yield('content')
+    </main>
+
+    <footer id='footer'>Grupo EngeSoft | Analise 2<br>2019.2</footer>
+    </div>
+
+    <script>
+        function move()
+        {
+            if (document.getElementById('check').checked)
+            {
+                document.querySelector('.pagebody').style.marginLeft  = '275px';
+            } else {
+                document.querySelector('.pagebody').style.marginLeft  = '25px';
+            }
+        }
+    </script>
 </body>
+
 </html>
